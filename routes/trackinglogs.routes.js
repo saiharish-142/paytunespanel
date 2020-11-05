c4f72aa524db84fbbbef66a220c5bb9b0b5379cc
Priority-Pulse-org <prioritypulse@gmail.com>
2020-11-05 14:26:13 +0530

    Update trackinglogs.routes.js

--- routes/trackinglogs.routes.js
+++ routes/trackinglogs.routes.js
@@ -76,7 +76,8 @@ router.post('/logbtdet/:num',adminauth,(req,res)=>{
     .skip(1000*num)
     .then(result=>{
         data = result
-        data = data.filter(x => x.campaignId === req.body.campaignId && x.createdOn < dat2)
+        data = data.filter(x => x.campaignId === req.body.campaignId)
+        data = data.filter(x => x.createdOn < dat2)
         if(!result.length){
             return res.status(422).json({error:"not found",result})
         }
