c9b5b841701ee76fb252507f80c5b8e192a5973e
Priority-Pulse-org <prioritypulse@gmail.com>
2020-11-06 14:17:36 +0530

    Update trackinglogs.routes.js

--- routes/trackinglogs.routes.js
+++ routes/trackinglogs.routes.js
@@ -66,125 +66,6 @@ router.post('/logcamp/:num',adminauth,(req,res)=>{
     })
 })
 
-router.post('/creareport',adminauth,(req,res)=>{
-    const { campaignId, date, tdate } = req.body
-    var dat = new Date(date)
-    var tdat = new Date(tdate)
-    var Ldata = [];
-    var data = [];
-    var fdata = [];
-    var i=0;
-    console.log('started',dat,tdat)
-    async function reportMaker(){
-        trackinglogs.find({createdOn:{$lte:dat}})
-        .sort('-createdOn')
-        .limit(1000)
-        .skip(1000*i)
-        .then(async (result)=>{
-            data = result
-            data = await data.filter(x=>x.campaignId.equals(campaignId))
-            Ldata = await data.filter(x=>{
-                var d = new Date(x.createdOn)
-                if(d<=tdat){
-                    return x;
-                }
-            })
-            fdata = fdata.concat(data)
-            console.log(data.length,`completed round ${i} in campaign`,fdata.length)
-            i++;
-            // if(result.length===0 || Ldata.length>1){
-                // clearInterval(timer)
-                if(fdata.length===0)
-                console.log('noo logs found')
-                if(fdata.length>0){
-                    // publisherfinder(fdata,date,campaignId)
-                }
-                return res.json({message:'no more logs available',fdata,data})
-            // }else{console.log('done')}
-            // region = await data.map(log => {return log.region })
-        })
-        .catch(err => {
-            console.log(err)
-            clearInterval(timer)
-            // publisherfinder(fdata,date,campaignId)
-        })
-    }
-    var timer = setInterval(reportMaker, 300000)
-})
-
-async function publisherfinder({logs,date,campaignId}){
-    var app = [];
-    var applogs = [];
-    var i = 0;
-    function removeDuplicates(originalArray, prop) {
-        var newArray = [];
-        var lookupObject  = {};
-        for(var i in originalArray) {
-            lookupObject[originalArray[i][prop]] = originalArray[i];
-        }
-    
-        for(i in lookupObject) {
-            newArray.push(lookupObject[i]);
-        }
-        return newArray;
-    }
-    
-    // var logs = await removeDuplicates(jlogs, "_id");
-    publisherapps.find()
-    .then(async (result)=>{
-        app = await result.map(x => x._id)
-        async function midware(){
-            applogs = await logs.filter(x => x.appId.equals(app[i]))
-            if(app[i]!== undefined){
-            console.log(app[i],logs)
-            reportposter(applogs,app[i],date,campaignId)}
-            if(i>=app.length){
-                clearInterval(tim)
-            }
-            i++;
-        }
-        var tim = setInterval(midware, 3000)
-    })
-    .catch(err => console.log(err))
-}
-
-async function reportposter({logsFiltered,appid,date,campaignId}){
-    var region = [];
-    var complete = [];
-    var clicked = [];
-    var clicked2 = [];
-    var impressions = [];
-    // var start = [];
-    region = await logsFiltered.map(x => { return x.region })
-    region = [...new Set(region)];
-    complete = await logsFiltered.filter(x => x.type==='complete')
-    impressions = await data.filter(x => x.type==='impression')
-    // start = await data.filter(x => x.type==='start')
-    if(appid.equals('5f91ca4441375c24943f4756')){
-        clicked = logsFiltered.filter(x=>x.type==='clicktracking')
-        clicked2 = logsFiltered.filter(x => x.type==='click')
-        clicked = clicked.concat(clicked2)
-    }else{
-        clicked = logsFiltered.filter(x=>x.type==='companionclicktracking')
-        clicked2 = logsFiltered.filter(x => x.type==='click')
-        clicked = clicked.concat(clicked2)
-    }
-    const report = new Report({
-        date:date,
-        Publisher:appid,
-        campaignId:campaignId,
-        impressions:impressions.length,
-        complete:complete.length,
-        clicks:clicked.length,
-        region:region,
-        Spend:logsFiltered.length + ' USD',
-        avgSpend:logsFiltered.length
-    })
-    report.save()
-    .then(reul => console.log(reul))
-    .catch(err => console.log(err))
-}
-
 router.post('/logbtdet/:num',adminauth,(req,res)  =>{
     var data = [];
     var data2 = [];
@@ -192,10 +73,10 @@ router.post('/logbtdet/:num',adminauth,(req,res)  =>{
     var dat2 = new Date(req.body.date2)
     const num = req.params.num
     const { campaignId } = req.body
-    var ob =  new ObjectId(campaignId)
+    // var ob =  new ObjectId(campaignId)
     trackinglogs.find({
-        createdOn:{$lte:dat},
-        campaignId:ob
+        createdOn:dat,
+        campaignId:campaignId
     })
     .sort('-createdOn')
     .limit(1000)
