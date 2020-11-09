477b56cec50d057cebf46cab2c583451c69bf8e4
Priority-Pulse-org <prioritypulse@gmail.com>
2020-11-10 01:02:05 +0530

    Update TitlRname.js

--- client/src/components/TitlRname.js
+++ client/src/components/TitlRname.js
@@ -5,16 +5,12 @@ function TitlRname({title,settitle,submit,setloading,loading}) {
     return (
         <div style={{display:"flex",justifyContent:'center'}}>
             <Paper style={{width:'60%',margin:'0 0 20px 0'}}>
-                {loading ? <div style={{margin:'20px'}}>loading....</div> : <form onSubmit={e=>{
-                    e.preventDefault()
-                    setloading(true)
-                    submit(title)
-                }}>
+                {loading ? <div style={{margin:'20px'}}>loading....</div> : <>
                     <input value={title} placeholder='Ad Title' onChange={e=>settitle(e.target.value)} style={{width:"70%",margin:'0 20px 10px 20px'}} />
                     <button className='btn' onClick={()=>{
                         setloading(true)
                         submit(title)
-                    }}>rename</button></form>
+                    }}>rename</button></>
                 }
             </Paper>
         </div>
