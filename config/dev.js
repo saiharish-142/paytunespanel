029737937effd8b7eb2b947f17d583d5a33d65c6
Priority-Pulse-org <prioritypulse@gmail.com>
2020-11-16 19:39:05 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,4 +1,5 @@
 module.exports={
-    MONGOURI:'mongodb://127.0.0.1/paytunesmusicadsdbs',
+    // MONGOURI:'mongodb+srv://sai:sai@cluster0.qsl2u.mongodb.net/paytunesmusic?authSource=admin&replicaSet=atlas-5fjq4b-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true',
+    MONGOURI:'mongodb://rtbhgk:GaurRajTiwari74546447928023823823@18.163.155.236:27017/paytunesmusic',
     JWT_SECRET:'guib91298hinx91208hi9nxk'
 }
