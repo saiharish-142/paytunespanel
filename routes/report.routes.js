bbccce569bdf36803233162521ccf6b5d840795e
Priority-Pulse-org <prioritypulse@gmail.com>
2020-11-17 23:08:52 +0530

    Update report.routes.js

--- routes/report.routes.js
+++ routes/report.routes.js
@@ -103,7 +103,7 @@ router.put('/sumreportofcam22',adminauth,(req,res)=>{
     var resu = [];
     Report.aggregate([
         {$match:{
-            "campaignId":{$in:[campaignId]}
+            "campaignId":{$in:campaignId}
         }},{$group:{
             _id:"$Publisher", impressions:{$sum:"$impressions"}, complete:{$sum:"$complete"}, clicks:{$sum:"$clicks"}, region:{$push:"$region"}
         }},{$project:{
