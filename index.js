7b249ade3add4e2eb09d10d97d7e5da6cc014c95
Priority-Pulse-org <prioritypulse@gmail.com>
2020-11-21 20:51:20 +0530

    damge control

--- index.js
+++ index.js
@@ -43,103 +43,106 @@ if(process.env.NODE_ENV==="production"){
         res.sendFile(path.resolve(__dirname,'client','build','index.html'))
     })
 }
+
 // var d = new Date()
-//     d.setDate(d.getDate()-1);
-//     if(d.getDate() < 10){
-//         var dte = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + '0' + d.getDate()
-//     }else{
-//         var dte = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate()
-//     }
+    //     d.setDate(d.getDate()-1);
+    //     if(d.getDate() < 10){
+    //         var dte = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + '0' + d.getDate()
+    //     }else{
+    //         var dte = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate()
+    //     }
     // var yd = new Date(dte)
     // console.log(dte)
 app.listen(port, () => console.log(`app listening on port ${port}!`))
 
 // cron.schedule('20 00 * * *', function() {
-//     var d = new Date()
-//     d.setDate(d.getDate()-1);
-//     if(d.getDate() < 10){
-//         var date = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + '0' + d.getDate()
-//     }else{
-//         var date = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate()
-//     }
-//     const trackinglogs = mongoose.model('trackinglogs')
-//     var data = [];
-//     trackinglogs.aggregate([
-//         { $match: {
-//             "date":date,
-//             "type":{$in:["impression","complete","click","companionclicktracking","clicktracking"]}
-//         } },
-//         { $group:{
-//             _id: {date:"$date" ,campaignId:"$campaignId" ,appId: "$appId",region :"$region",type:"$type"},count:{$sum:1}
-//         }},{$group:{
-//             _id:{date:"$_id.date" ,campaignId:"$_id.campaignId" ,appId:"$_id.appId",type:"$_id.type"} , region:{$push:"$_id.region"}, count:{$sum:"$count"}
-//         }},{$group:{
-//             _id:{date:"$_id.date" ,campaignId:"$_id.campaignId" ,appId:"$_id.appId"}, type:{$push:{type:"$_id.type",count:"$count"}}, region:{$push:"$region"}
-//         }},{$group:{
-//             _id:{date:"$_id.date" ,campaignId:"$_id.campaignId"}, report:{$push:{appId:"$_id.appId", type:"$type", region:"$region"}}
-//         }},{$project:{
-//             date: "$_id.date",campaignId:"$_id.campaignId", report:"$report", _id:0
-//         }}
-//     ])
-//     .then(result=>{
-//         data = result;
-//         data.map((det)=>{
-//             console.log(det.campaignId)
-//             det.report.map(camrepo=>{
-//                 var resregion = [].concat.apply([], camrepo.region);
-//                 resregion = [...new Set(resregion)];
-//                 camrepo.region = resregion
-//             })
-//         })
-//         var compr = [];
-//         for(var i=0; i<data.length; i++ ){
-//             const Report = mongoose.model('Report')
-//             var cam = data[i].campaignId ;
-//             var da = data[i].date ;
-//             for(var j=0;j<data[i].report.length;j++){
-//                 var impre = 0;
-//                 var compl = 0;
-//                 var click = 0;
-//                 data[i].report[j].type.map(repo => {
-//                     if(repo.type==='impression'){
-//                         impre += repo.count
-//                     }
-//                     if(repo.type==='complete'){
-//                         compl += repo.count
-//                     }
-//                     if(repo.type==='companionclicktracking'){
-//                         click += repo.count
-//                     }
-//                     if(repo.type==='clicktracking'){
-//                         click += repo.count
-//                     }
-//                     if(repo.type==='click'){
-//                         click += repo.count
-//                     }
-//                 })
-//                 const report = new Report({
-//                     date:da,
-//                     Publisher:data[i].report[j].appId,
-//                     campaignId:cam,
-//                     impressions:impre,
-//                     complete:compl,
-//                     clicks:click,
-//                     region:data[i].report[j].region,
-//                     spend:impre,
-//                     avgSpend:impre
-//                 })
-//                 report.save()
-//                 .then(result => {
-//                     compr.push(result)
-//                     console.log("completed")
-//                 })
-//                 .catch(err => console.log(err))
-//             }
-//         }
-//         // res.json(compr)
-//     })
-//     .catch(err => console.log(err))
-// });
+    //     var d = new Date()
+    //     d.setDate(d.getDate()-1);
+    //     if(d.getDate() < 10){
+    //         var date = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + '0' + d.getDate()
+    //     }else{
+    //         var date = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate()
+    //     }
+    //     const trackinglogs = mongoose.model('trackinglogs')
+    //     var data = [];
+    //     trackinglogs.aggregate([
+    //         { $match: {
+    //             "date":date,
+    //             "type":{$in:["impression","complete","click","companionclicktracking","clicktracking"]}
+    //         } },
+    //         { $group:{
+    //             _id: {date:"$date" ,campaignId:"$campaignId" ,appId: "$appId",region :"$region",type:"$type"},count:{$sum:1}
+    //         }},{$group:{
+    //             _id:{date:"$_id.date" ,campaignId:"$_id.campaignId" ,appId:"$_id.appId",type:"$_id.type"} , region:{$push:"$_id.region"}, count:{$sum:"$count"}
+    //         }},{$group:{
+    //             _id:{date:"$_id.date" ,campaignId:"$_id.campaignId" ,appId:"$_id.appId"}, type:{$push:{type:"$_id.type",count:"$count"}}, region:{$push:"$region"}
+    //         }},{$group:{
+    //             _id:{date:"$_id.date" ,campaignId:"$_id.campaignId"}, report:{$push:{appId:"$_id.appId", type:"$type", region:"$region"}}
+    //         }},{$project:{
+    //             date: "$_id.date",campaignId:"$_id.campaignId", report:"$report", _id:0
+    //         }}
+    //     ])
+    //     .then(result=>{
+    //         data = result;
+    //         data.map((det)=>{
+    //             console.log(det.campaignId)
+    //             det.report.map(camrepo=>{
+    //                 var resregion = [].concat.apply([], camrepo.region);
+    //                 resregion = [...new Set(resregion)];
+    //                 camrepo.region = resregion
+    //             })
+    //         })
+    //         var compr = [];
+    //         for(var i=0; i<data.length; i++ ){
+    //             const Report = mongoose.model('Report')
+    //             var cam = data[i].campaignId ;
+    //             var da = data[i].date ;
+    //             for(var j=0;j<data[i].report.length;j++){
+    //                 var impre = 0;
+    //                 var compl = 0;
+    //                 var click = 0;
+    //                 data[i].report[j].type.map(repo => {
+    //                     if(repo.type==='impression'){
+    //                         impre += repo.count
+    //                     }
+    //                     if(repo.type==='complete'){
+    //                         compl += repo.count
+    //                     }
+    //                     if(repo.type==='companionclicktracking'){
+    //                         click += repo.count
+    //                     }
+    //                     if(repo.type==='clicktracking'){
+    //                         click += repo.count
+    //                     }
+    //                     if(repo.type==='click'){
+    //                         click += repo.count
+    //                     }
+    //                 })
+    //                 const report = new Report({
+    //                     date:da,
+    //                     Publisher:data[i].report[j].appId,
+    //                     campaignId:cam,
+    //                     impressions:impre,
+    //                     complete:compl,
+    //                     clicks:click,
+    //                     region:data[i].report[j].region,
+    //                     spend:impre,
+    //                     avgSpend:impre
+    //                 })
+    //                 report.save()
+    //                 .then(result => {
+    //                     compr.push(result)
+    //                     console.log("completed")
+    //                 })
+    //                 .catch(err => console.log(err))
+    //             }
+    //         }
+    //         // res.json(compr)
+    //     })
+    //     .catch(err => console.log(err))
+    // });
+
+
 
 cron.schedule('00 02 * * *', function(){
     var d = new Date()
@@ -440,8 +443,9 @@ function ReportsRefresher(date,credate){
     })
     .catch(err => console.log(err))
 }
-// var currentTime = new Date();
-// var currentOffset = currentTime.getTimezoneOffset();
-// var ISTOffset = 330;   // IST offset UTC +5:30 
-// var ISTTime = new Date(currentTime.getTime() + (ISTOffset*2 + currentOffset -5)*60000);
-// console.log(ISTTime)
+
+var currentTime = new Date();
+var currentOffset = currentTime.getTimezoneOffset();
+var ISTOffset = 330;   // IST offset UTC +5:30 
+var ISTTime = new Date(currentTime.getTime() + (ISTOffset*2 + currentOffset -5)*60000);
+console.log(ISTTime)
--- routes/trackinglogs.routes.js
+++ routes/trackinglogs.routes.js
@@ -82,6 +82,121 @@ router.post('/suspect',adminauth,(req,res)=>{
     })
 })
 
+router.put('/repotest',adminauth,(req,res)=>{
+    var d = new Date()
+    d.setDate(d.getDate());
+    if(d.getDate() < 10){
+        var date = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + '0' + d.getDate()
+    }else{
+        var date = d.getFullYear() + '-' + (d.getMonth()+1) + '-' + d.getDate()
+    }
+    var currentTime = new Date();
+    var currentOffset = currentTime.getTimezoneOffset();
+    var ISTOffset = 330;   // IST offset UTC +5:30 
+    var ISTTime = new Date(currentTime.getTime() + (ISTOffset*2 + currentOffset -5)*60000);
+    console.log(ISTTime)
+    // ReportsRefresher(date,ISTTime)
+    console.log(date,credate)
+    const trackinglogs = mongoose.model('trackinglogs')
+    var data = [];
+    trackinglogs.aggregate([
+        { $match: {
+            "date":date,
+            "type":{$in:["impression","complete","click","companionclicktracking","clicktracking"]},
+            "createdOn":{$lte : ISTTime}
+        } },
+        { $group:{
+            _id: {date:"$date" ,campaignId:"$campaignId" ,appId: "$appId",region :"$region",type:"$type"},count:{$sum:1}
+        }},{$group:{
+            _id:{date:"$_id.date" ,campaignId:"$_id.campaignId" ,appId:"$_id.appId",type:"$_id.type"} , region:{$push:"$_id.region"}, count:{$sum:"$count"}
+        }},{$group:{
+            _id:{date:"$_id.date" ,campaignId:"$_id.campaignId" ,appId:"$_id.appId"}, type:{$push:{type:"$_id.type",count:"$count"}}, region:{$push:"$region"}
+        }},{$group:{
+            _id:{date:"$_id.date" ,campaignId:"$_id.campaignId"}, report:{$push:{appId:"$_id.appId", type:"$type", region:"$region"}}
+        }},{$project:{
+            date: "$_id.date",campaignId:"$_id.campaignId", report:"$report", _id:0
+        }}
+    ])
+    .then(result=>{
+        data = result;
+        data.map((det)=>{
+            console.log(det.campaignId)
+            det.report.map(camrepo=>{
+                var resregion = [].concat.apply([], camrepo.region);
+                resregion = [...new Set(resregion)];
+                camrepo.region = resregion
+            })
+        })
+        console.log(data)
+        var compr = [];
+        for(var i=0; i<data.length; i++ ){
+            const Report = mongoose.model('Report')
+            var cam = data[i].campaignId ;
+            var da = data[i].date ;
+            for(var j=0;j<data[i].report.length;j++){
+                var impre = 0;
+                var compl = 0;
+                var click = 0;
+                data[i].report[j].type.map(repo => {
+                    if(repo.type==='impression'){
+                        impre += repo.count
+                    }
+                    if(repo.type==='complete'){
+                        compl += repo.count
+                    }
+                    if(repo.type==='companionclicktracking'){
+                        click += repo.count
+                    }
+                    if(repo.type==='clicktracking'){
+                        click += repo.count
+                    }
+                    if(repo.type==='click'){
+                        click += repo.count
+                    }
+                })
+                Report.findOneAndUpdate({campaignId:cam,date:da})
+                .then(foundreport=>{
+                    if(!foundreport){
+                        const report = new Report({
+                            date:da,
+                            Publisher:data[i].report[j].appId,
+                            campaignId:cam,
+                            impressions:impre,
+                            complete:compl,
+                            clicks:click,
+                            region:data[i].report[j].region,
+                            spend:impre,
+                            avgSpend:impre
+                        })
+                        report.save()
+                        .then(result => {
+                            compr.push(result)
+                            console.log("completed")
+                        })
+                        .catch(err => console.log(err))
+                    }else{
+                        foundreport.date = da;
+                        foundreport.Publisher = data[i].report[j].appId;
+                        foundreport.campaignId = cam;
+                        foundreport.impressions = impre;
+                        foundreport.complete = compl;
+                        foundreport.clicks = click;
+                        foundreport.region = data[i].report[j].region;
+                        foundreport.spend = impre;
+                        foundreport.avgSpend = impre;
+                        foundreport.save()
+                        .then(ree=> console.log('updated'))
+                        .catch(err => console.log(err))
+                    }
+                })
+                .catch(err => console.log(err))
+            }
+        }
+        // res.json(compr)
+    })
+    .catch(err => console.log(err))
+})
+
 router.post('/logcamp/:num',adminauth,(req,res)=>{
     const num = req.params.num
     trackinglogs.find({campaignId:req.body.campaignId})
