0bb6f165dc37af65fa8957dfb137cbe493080ce2
Priority-Pulse-org <prioritypulse@gmail.com>
2020-11-29 22:44:59 +0530

    Update adsetting.routes.js

--- routes/adsetting.routes.js
+++ routes/adsetting.routes.js
@@ -6,8 +6,19 @@ const adsetting = mongoose.model('adsetting')
 
 router.get('/all',adminauth,(req,res)=>{
     adsetting.find()
+    .sort('-createdOn')
     .then(result=>res.json(result))
     .catch(err=>res.status(400).json(err))
+})
+
+router.put('/addetails',adminauth,(req,res)=>{
+    const { campaignId } = req.body
+    adsetting.find({campaignId:{$in:campaignId}})
+    .sort('-createdOn')
+    .then(result=>{
+        res.json(result)
+    })
+    .catch(err => res.status(400).json(err))
 })
 
 module.exports = router
