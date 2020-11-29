8507ff2581d987ff3f12e31ccf343ebe43e2e04f
Priority-Pulse-org <prioritypulse@gmail.com>
2020-11-29 23:35:12 +0530

    Update adsetting.routes.js

--- routes/adsetting.routes.js
+++ routes/adsetting.routes.js
@@ -13,7 +13,11 @@ router.get('/all',adminauth,(req,res)=>{
 
 router.put('/addetail',adminauth,(req,res)=>{
     const { campaignId } = req.body
-    adsetting.find({campaignId:{$in:campaignId}})
+    var ids = (typeof campaignId !== 'undefined' && 
+                typeof campaignId !== 'string' && 
+                typeof campaignId !== 'object') ? 
+                campaignId.map(id=>mongoose.Types.ObjectId(id)) : campaignId
+    adsetting.find({campaignId:{$in:ids}})
     .sort('-createdOn')
     .then(result=>{
         res.json(result)
@@ -31,7 +35,33 @@ router.put('/addetails',adminauth,(req,res)=>{
         {$match:{
             "campaignId":{$in:ids}
         }},{$group:{
-            _id:"$type", ids:{$push:"$_id"}
+            _id:"$type", ids:{$push:"$campaignId"}
+        }},{$project:{
+            type:"$_id",_id:0, ids:"$ids"
+        }}
+    ])
+    .then(result=>{
+        var doodle = result;
+        var audio = [];
+        var display = [];
+        doodle.map(sub=>{
+            if(sub.type === 'audio')
+            audio = audio.concat(sub.ids)
+            if(sub.type === 'display')
+            display = display.concat(sub.ids)
+        })
+        res.json({audio,display})
+    })
+    .catch(err => res.status(400).json(err))
+})
+
+router.put('/addetails',adminauth,(req,res)=>{
+    const { campaignId } = req.body
+    adsetting.aggregate([
+        {$match:{
+            "campaignId":{$in:campaignId}
+        }},{$group:{
+            _id:"$type", ids:{$push:"$campaignId"}
         }},{$project:{
             type:"$_id",_id:0, ids:"$ids"
         }}
