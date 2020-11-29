ff6e47f4035c136c6c820f2e12a275c18cb5789a
Priority-Pulse-org <prioritypulse@gmail.com>
2020-11-30 00:57:01 +0530

    Update adsetting.routes.js

--- routes/adsetting.routes.js
+++ routes/adsetting.routes.js
@@ -44,7 +44,7 @@ router.put('/addetailt',adminauth,(req,res)=>{
             reu.map(rrr=>{
                 // console.log(rrr.campaignId.equals(id))
                 if(rrr.campaignId.equals(id)){
-                    console.log(rrr.type,'type')
+                    console.log(rrr.type,'type',rrr)
                     if(rrr.type==='audio'){
                         audio.push(id)
                         audimpression += parseInt(rrr.targetImpression)
