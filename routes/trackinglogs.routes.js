9b4485d91a1a41b204f86e81a464621f6f0d4e09
Priority-Pulse-org <prioritypulse@gmail.com>
2020-12-11 17:59:21 +0530

    Update trackinglogs.routes.js

--- routes/trackinglogs.routes.js
+++ routes/trackinglogs.routes.js
@@ -464,7 +464,7 @@ router.post('/testcom1',adminauth,async (req,res)  =>{
                             "initialValue": [],
                             "in": { "$concatArrays": [ "$$value", "$$this" ] }
                 }}}},
-                {$project:{_id:1,result:1,unique:1}},
+                {$project:{_id:1,result:1,unique:{$addToSet:"$unique"}}},
                 {$group:{_id:{appId:"$_id.appId",campaignId:"$_id.campaignId",rtbType:"$_id.rtbType"}, result:{$push:{region:"$_id.region",unique:{$size:"$unique"},result:{$arrayToObject:"$result"}}}}},
                 {$group:{_id:"$_id.campaignId",report:{$push:{appId:"$_id.appId",rtbType:"$_id.rtbType",result:"$result"}}}},
                 {$project:{_id:0,campaignId:"$_id",report:"$report"}}
