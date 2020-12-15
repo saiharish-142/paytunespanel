2c73f7d5db5f2a846760390fa69e6a097193fd7b
Priority-Pulse-org <prioritypulse@gmail.com>
2020-12-15 18:09:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,17 +10,13 @@ const cron = require('node-cron')
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect(MONGOURI,{
+const options = {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    autoIndex: false, // Don't build indexes
-    poolSize: 10, // Maintain up to 10 socket connections
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-    socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-    family: 4 // Use IPv4, skip trying IPv6
-})
+    serverSelectionTimeoutMS: 500000 
+}
+
+mongoose.connect(MONGOURI,options)
 mongoose.connection.on('connected',() => {
     console.log("connected to database.....")
 })
