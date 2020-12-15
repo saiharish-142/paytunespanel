228540024f159455ebc95ffcae1d4ac502f3551a
Priority-Pulse-org <prioritypulse@gmail.com>
2020-12-15 19:13:10 +0530

    Update trackinglogs.routes.js

--- routes/trackinglogs.routes.js
+++ routes/trackinglogs.routes.js
@@ -567,16 +567,8 @@ router.post('/testcom2',adminauth,async (req,res)  =>{
     const { campaignId, date, date2 } = req.body
     var resu = [];
     var newdate = new Date(date2)
-    StreamingAds.aggregate([
-        {$match:{"endDate":{$gte:newdate},"startDate":{$lte:newdate}}},
-        {$group:{_id:null,ids:{$push:"$_id"}}},
-        {$project:{_id:0,ids:"$ids"}}
-    ])
-    .then(result=>{
-        resu = result;
-        res.json(resu)
-    })
-    .catch(err => console.log(err))
+    let number = await trackinglogs.distinct('ifa',{"campaignId":"5f736a8602a2051e8de33360","appId": "5d3f052e979a1c2391016c04","type":"impression"}).length
+    res.json(number)
 })
 
 router.post('/testcom3',adminauth,async (req,res)  =>{
