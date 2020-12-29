aaf65dd2a194be4288d27af3151c9b71c00009b6
Priority-Pulse-org <prioritypulse@gmail.com>
2020-12-29 20:07:56 +0530

    Update uniqueuser.model.js

--- models/uniqueuser.model.js
+++ models/uniqueuser.model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const uniqueSchema = new mongoose.Schema({
     audiouser:{type:Number},
     displayuser:{type:Number},
-    AdTitle:{type:String},
+    AdTitle:{type:String,unique:true},
 })
 
 mongoose.model('unique',uniqueSchema)
