f36ffc37b6465f90cf81475a2d95cc8123ca9421
Priority-Pulse-org <prioritypulse@gmail.com>
2021-02-13 16:11:31 +0530

    Update phonemakereports.routes.js

--- routes/phonemakereports.routes.js
+++ routes/phonemakereports.routes.js
@@ -14,7 +14,22 @@ router.get('/',adminauth,(req,res)=>{
 router.put('/reportbycampids',adminauth,(req,res)=>{
     const {campaignId} = req.body
     var ids = campaignId.map(id=>mongoose.Types.ObjectId(id))
-    phonemakereports.find({campaignId:{$in:ids}})
+    phonemakereports.aggregate([
+        {$match:{campaignId:{$in:ids}}},
+        {$group:{_id:{phoneMake:"$phoneMake",campaignId:"$campaignId"}, 
+            impression:{$sum:"$impression"}, 
+            CompanionClickTracking:{$sum:"$CompanionClickTracking"}, 
+            SovClickTracking:{$sum:"$SovClickTracking"}, 
+            start:{$sum:"$start"}, 
+            midpoint:{$sum:"$midpoint"},
+            thirdQuartile:{$sum:"$thirdQuartile"},
+            complete:{$sum:"$complete"},
+            createdOn:{$push:"$createdOn"}
+        }},{$project:{
+            phoneMake:"$_id.phoneMake", campaignId:"$_id.campaignId",impression:1,CompanionClickTracking:1,SovClickTracking:1,
+            start:1,midpoint:1,thirdQuartile:1,complete:1,createdOn:1,_id:0
+        }}
+    ])
     .then(result=>res.json(result))
     .catch(err=>res.status(422).json(err))
 })
