889006bfa1755ae999905c3ad00fa6d0d3aa8901
Priority-Pulse-org <saiharishmedam@gmail.com>
2021-05-18 21:16:03 +0530

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { Provider } from "react-redux";
+import { Provider } from 'react-redux';
 import store from './redux';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
-
