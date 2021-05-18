df6f7a6a926dc658738c5813cc08693851c951f8
Priority-Pulse-org <saiharishmedam@gmail.com>
2021-05-18 21:34:10 +0530

    2

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import store from './redux';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
+import Apindex from './apindex';
 
 ReactDOM.render(
 	<BrowserRouter>
 		<React.StrictMode>
-			<Provider store={store}>
-				<App />
-			</Provider>
+			<Apindex />
 		</React.StrictMode>
 	</BrowserRouter>,
 	document.getElementById('root')
