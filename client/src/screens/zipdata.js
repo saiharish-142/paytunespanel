cdd082d9a87f411f25c828713bbaac3db6764849
kritikdalakoti <dalakotikritik@gmail.com>
2021-05-26 21:11:28 +0530

    editted zipdata panel

--- client/src/screens/zipdata.js
+++ client/src/screens/zipdata.js
@@ -135,6 +135,7 @@ export default function Zipdata() {
 							<TableRow>
 								{/* <TableCell>{title}</TableCell> */}
 								{<TableCell>Pincode</TableCell>}
+								{<TableCell>Impressions</TableCell>}
 								{<TableCell>Urban/Rural</TableCell>}
 								{<TableCell>Lower Sub City</TableCell>}
 								<TableCell>Subcity</TableCell>
@@ -146,6 +147,7 @@ export default function Zipdata() {
 								{<TableCell>Grand State</TableCell>}
 								{<TableCell>Lat</TableCell>}
 								{<TableCell>Long</TableCell>}
+								{<TableCell>Click</TableCell>}
 								{<TableCell />}
 							</TableRow>
 						</TableHead>
@@ -157,6 +159,7 @@ export default function Zipdata() {
 										<TableCell component="th" scope="row">
 											{row.pincode ? row.pincode : ''}
 										</TableCell>
+										<TableCell>{row.impression ? row.impression : ''}</TableCell>
 										<TableCell>{row.area ? row.area : ''}</TableCell>
 										<TableCell>{row.lowersubcity ? row.lowersubcity : ''}</TableCell>
 										<TableCell>{row.subcity ? row.subcity : ''}</TableCell>
@@ -168,6 +171,7 @@ export default function Zipdata() {
 										<TableCell>{row.grandstate ? row.grandstate : ''}</TableCell>
 										<TableCell>{row.latitude ? row.latitude : ''}</TableCell>
 										<TableCell>{row.longitude ? row.longitude : ''}</TableCell>
+										<TableCell>{row.click ? row.click : ''}</TableCell>
 										<TableCell>
 											<button className="btn" onClick={() => handleShow(row)}>
 												Edit{' '}
