cdbb944de25a6608cba2210d9310d8b26e36e991
kritikdalakoti <dalakotikritik@gmail.com>
2021-07-03 10:51:11 +0530

    fixing episode taba

--- client/src/components/episodestab.js
+++ client/src/components/episodestab.js
@@ -263,7 +263,7 @@ export default function EpisodeTab(){
 								{<TableCell style={{ cursor: 'pointer' }} onClick={()=>requestSort('publisher')} className={getClassNamesFor('publisher')}> Publisher {arrowRetuner( sortconfig.key==='publisher'?(sortconfig.direction==='ascending'?'1':'2'):'3' )} </TableCell>}
 								{<TableCell style={{ cursor: 'pointer' }} onClick={()=>requestSort('category')} className={getClassNamesFor('category')}> Category {arrowRetuner( sortconfig.key==='category'?(sortconfig.direction==='ascending'?'1':'2'):'3' )} </TableCell>}
 								{<TableCell style={{ cursor: 'pointer' }} onClick={()=>requestSort('displayname')} className={getClassNamesFor('displayname')}> Display Name {arrowRetuner( sortconfig.key==='displayname'?(sortconfig.direction==='ascending'?'1':'2'):'3' )} </TableCell>}
-								{<TableCell style={{ cursor: 'pointer' }} onClick={()=>requestSort('hostpossibility')} className={getClassNamesFor('hostpossibility')}> Host Possibility {arrowRetuner( sortconfig.key==='hostPossibility'?(sortconfig.direction==='ascending'?'1':'2'):'3' )} </TableCell>}
+								{<TableCell style={{ cursor: 'pointer' }} onClick={()=>requestSort('hostPossibility')} className={getClassNamesFor('hostPossibility')}> Host Possibility {arrowRetuner( sortconfig.key==='hostPossibility'?(sortconfig.direction==='ascending'?'1':'2'):'3' )} </TableCell>}
 								{<TableCell />}
 							</TableRow>
 						</TableHead>
@@ -276,8 +276,8 @@ export default function EpisodeTab(){
 									<TableCell>{row.request ? row.request : ''}</TableCell>
 									<TableCell>{row.publisher ? row.publisher: ''}</TableCell>
 									<TableCell>{row.category ? '' : ''}</TableCell>
-									<TableCell>{row.category ? row.displayname : ''}</TableCell>
-									<TableCell>{row.category ? row.hostPossibility : ''}</TableCell>
+									<TableCell>{row.displayname ? row.displayname : ''}</TableCell>
+									<TableCell>{row.hostPossibility ? row.hostPossibility : ''}</TableCell>
 									<TableCell>
 										<button className="btn" onClick={() => handleOpen(row)}>  
 											Edit{' '}
