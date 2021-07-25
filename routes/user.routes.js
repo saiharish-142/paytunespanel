3c8fb79df2545cdef0d269d24cffc9294b76129c
Priority-Pulse-org <saiharishmedam@gmail.com>
2021-07-25 19:56:07 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -136,10 +136,16 @@ router.get('/loggedUser', adminauth, (req, res, next) => {
 router.put('/addbundleOrcampaigns', adminauth, (req, res) => {
 	const { id, campaigns, bundles } = req.body;
 	// console.log(req.body);
+	if (!id || !campaigns || !bundles) {
+		return res.status(422).json({ error: 'Enter all the required details' });
+	}
 	admin
 		.findOne({ _id: id })
 		.then((user) => {
-			console.log(user);
+			if (!user) {
+				return res.status(422).json({ error: 'USer Not found' });
+			}
+			// console.log(user);
 			var setcamp = [ ...new Set(campaigns) ];
 			user.campaigns = setcamp;
 			var setbund = [ ...new Set(bundles) ];
