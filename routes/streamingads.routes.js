b92a51ea36e8f0c919bc69f8e6d64be68fb0749a
Priority-Pulse-org <saiharishmedam@gmail.com>
2021-08-12 10:27:33 +0530

    Update streamingads.routes.js

--- routes/streamingads.routes.js
+++ routes/streamingads.routes.js
@@ -400,10 +400,29 @@ router.get('/groupedMangename', adminauth, (req, res) => {
 });
 
 router.get('/clientcamps', adminauth, (req, res) => {
-	campaignClient.find({ userid: res.locals.user._id }).then((result) => res.json(result)).catch((err) => {
-		console.log(err);
-		res.status(400).json({ err, error: 'Something went wrong' });
-	});
+	campaignClient
+		.find({ userid: res.locals.user._id })
+		.then((result) => {
+			var sao = result;
+			sao.map((ad) => {
+				var remainingdays = 0;
+				var d1 = new Date(ad.endDate);
+				var d2 = new Date(Date.now());
+				// console.log(d1,d2)
+				var show = d1.getTime() - d2.getTime();
+				remainingdays = show / (1000 * 3600 * 24);
+				if (remainingdays < 0) {
+					remainingdays = 'completed campaign';
+				}
+				ad.remainingDays = remainingdays;
+				return ad;
+			});
+			res.json(sao);
+		})
+		.catch((err) => {
+			console.log(err);
+			res.status(400).json({ err, error: 'Something went wrong' });
+		});
 });
 
 router.put('/clientgrouped', adminauth, (req, res) => {
