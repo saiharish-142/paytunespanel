9103c5b88e3ca3b598e5cbdaa34311a4564e951f
Priority-Pulse-org <saiharishmedam@gmail.com>
2021-09-14 18:48:55 +0530

    Update streamingads.routes.js

--- routes/streamingads.routes.js
+++ routes/streamingads.routes.js
@@ -430,6 +430,7 @@ router.get('/clientcamps', adminauth, (req, res) => {
 router.get('/Acampaigns', adminauth, (req, res) => {
 	campaignClient
 		.find({})
+		.populate('userid')
 		.then((result) => {
 			var sao = result;
 			sao.map((ad) => {
