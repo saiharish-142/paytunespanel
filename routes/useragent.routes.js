eed866de0f9ad138cceccd3bfe1732ec9da0d1f4
Priority-Pulse-org <saiharishmedam@gmail.com>
2021-09-28 19:50:44 +0530

    Update useragent.routes.js

--- routes/useragent.routes.js
+++ routes/useragent.routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const adminauth = require('../authenMiddleware/adminauth');
 const useragentdata = mongoose.model('useragentdata');
 const uareqreports = mongoose.model('uareqreports');
