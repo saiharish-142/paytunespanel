6ad47fb96a6a80b97ac45e33979389408bd8855d
Priority-Pulse-org <saiharishmedam@gmail.com>
2021-12-15 18:29:59 +0530

    Update campaignwisereports.routes.js

--- routes/campaignwisereports.routes.js
+++ routes/campaignwisereports.routes.js
@@ -1308,6 +1308,7 @@ router.put('/sumreportofcamDiv', adminauth, (req, res) => {
 				$project: {
 					Publisher: '$appId',
 					PublisherSplit: '$_id.appubid',
+					publisherid: '$_id.appubid',
 					feed: '$_id.feed',
 					updatedAt: '$updatedAt',
 					ssp: '$ssp',
