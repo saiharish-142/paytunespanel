7dab0253c6e3c1cb5fc26c2f6d4e1226f04488a6
Priority-Pulse-org <saiharishmedam@gmail.com>
2021-12-16 19:27:40 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -220,6 +220,17 @@ router.post('/addCampaign', adminauth, async (req, res) => {
 		onDemand,
 		musicapps
 	} = req.body;
+	// console.log(
+	// 	!userid ||
+	// 		!searchName ||
+	// 		!endDate ||
+	// 		!startDate ||
+	// 		!type ||
+	// 		!campaignName ||
+	// 		!display ||
+	// 		!video ||
+	// 		!(!audio ? musicapps && podcast && musicapps : audio && musicapps && podcast && onDemand)
+	// );
 	if (
 		!userid ||
 		!searchName ||
@@ -229,7 +240,7 @@ router.post('/addCampaign', adminauth, async (req, res) => {
 		!campaignName ||
 		!display ||
 		!video ||
-		(!(!audio && (musicapps && podcast && onDemand)) || !(audio && musicapps && podcast && onDemand))
+		!(!audio ? musicapps && podcast && musicapps : audio && musicapps && podcast && onDemand)
 	) {
 		return res.status(422).json({ error: 'enter all the required fields' });
 	}
