7c6080ea3f17639a0d34bf4e96152b3e11a70139
Priority-Pulse-org <saiharishmedam@gmail.com>
2022-01-29 15:25:54 +0530

    Update tempModel1.model.js

--- models/tempModel1.model.js
+++ models/tempModel1.model.js
@@ -3,15 +3,11 @@ const { ObjectId } = mongoose.Schema.Types;
 
 const tempModel1Schema = new mongoose.Schema({
 	zip: { type: String },
-	campaignId: { type: String },
+	startDate: { type: String },
 	impression: { type: Number },
 	click: { type: Number },
-	firstquartile: { type: Number },
-	thirdquartile: { type: Number },
-	midpoint: { type: Number },
-	start: { type: Number },
 	complete: { type: Number }
 });
-tempModel1Schema.index({ campaignId: 1, time: 1 });
+tempModel1Schema.index({ zip: 1 });
 
 mongoose.model('tempModel1', tempModel1Schema);
